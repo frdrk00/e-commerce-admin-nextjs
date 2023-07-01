ae5ee8d85340ef6a6ddab09be002b9492dff12b2
frdrk00 <masterkooo@gmail.com>
2023-07-01 21:53:47 +0200

    Modal components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,23 @@
+'use client'
+
+import Modal from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal'
 import { UserButton } from '@clerk/nextjs'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
 interface SetupPageProps {}
 
 const SetupPage: FC<SetupPageProps> = ({}) => {
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  )
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+  
+  return <div className="p-4">Root Page</div>
 }
 
 export default SetupPage
